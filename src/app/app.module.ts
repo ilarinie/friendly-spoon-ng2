cf84bbfed3c3b95302002be1817e454a277be880
ilarinie <ilari.nieminen@outlook.com>
2016-08-18 12:10:24 +0300

    added declarations to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,34 @@ import { HttpModule } from '@angular/http';
 import { Authentication } from './authentication/authentication';
 
 
+import {RecipeListComponent} from "./recipe-list/recipe-list.component";
+import {RecipeComponent} from "./recipe/recipe.component";
+import {Tags} from "./recipe/tags/tags.component";
+import {Notes} from "./recipe/notes/notes.component";
+import { ListIngredients} from "./list-ingredients/list-ingredients.component";
+import {AddIngredients} from "./add-ingredients/add-ingredients.component";
+import {TinyEditor} from "./directives/tinymce.directive";
+import {AddRecipeIngredient} from "./add-recipe-ingredient/add-recipe-ingredient.component";
+
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        routing,
-        HttpModule
-    ],
-    bootstrap: [AppComponent],
-    providers: [Authentication],
+  declarations: [
+    AppComponent,
+    RecipeListComponent,
+    RecipeComponent,
+    Tags,
+    Notes,
+    ListIngredients,
+    AddIngredients,
+    TinyEditor,
+    AddRecipeIngredient,
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    HttpModule
+  ],
+  bootstrap: [AppComponent],
+  providers: [Authentication],
 })
 export class AppModule { }
