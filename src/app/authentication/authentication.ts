9930fcd86259d2ebb0179f7728574f11b819915a
ilarinie <ilari.nieminen@outlook.com>
2016-09-18 20:00:03 +0300

    changed api to ssl

--- src/app/authentication/authentication.ts
+++ src/app/authentication/authentication.ts
@@ -9,7 +9,7 @@ import { Router, CanActivate } from "@angular/router";
 export class Authentication implements CanActivate {
   private locationWatcher = new EventEmitter();
 
-  private apiUrl = "http://api.friendlyspoon.me"; //prod api
+  private apiUrl = "https://api.friendlyspoon.me"; //prod api
   //private apiUrl = 'http://friendly-spoon-api.herokuapp.com'; //dev api
   //private apiUrl = "http://localhost:3000";
 
