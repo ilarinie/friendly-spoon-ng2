38441a189f602a8be586c16a376a41b23336518f
ilarinie <ilari.nieminen@outlook.com>
2016-10-14 17:16:14 +0300

    fixed shopping cart

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,9 +1,9 @@
 
 
 export const Global = Object.freeze({
-  //apiUrl:'https://api.friendlyspoon.me'
+  apiUrl:'https://api.friendlyspoon.me'
   //apiUrl:'http://localhost:3000'
-  apiUrl: 'http://devapi.friendlyspoon.me'
+  //apiUrl: 'http://devapi.friendlyspoon.me'
   //apiUrl:'http://192.168.1.109:3000'
 
 })
